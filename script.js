fccf1f9d5c3aee831dcb7af36d84c88b5b0ff71e
XxAzatarXx1 <azatargaming@gmail.com>
2026-01-14 13:19:17 -0500

    Add player selection functionality and improve game logic

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const cells = document.querySelectorAll('.cell')
 const titleHeader = document.querySelector('#titleHeader')
 const xPlayerDisplay = document.querySelector('#xPlayerDisplay')
 const oPlayerDisplay = document.querySelector('#oPlayerDisplay')
+const restartBtn = document.querySelector('#restartBtn')
 
 
-let player = 'X'
+let player = 'x'
 let isPauseGame = false
 let isGameStart = false
 
@@ -15,8 +16,8 @@ const inputCells = ['', '', '',
 
 
 const winConditions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8]
-    [0, 3, 6], [1, 4, 7], [2, 5, 8]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ]             
 
@@ -31,6 +32,10 @@ function tapCell(cell, index) {
     ) {
         isGameStart = true
         updateCell(cell, index)
+        if (!checkWinner()) {
+        changePlayer()
+        randomPick()
+        }
     }
 }
 
@@ -38,5 +43,94 @@ function tapCell(cell, index) {
 function updateCell(cell, index) {
     cell.textContent = player
     inputCells[index] = player
-    cell.style.color = (player == 'X') ? '#1892EA' : '#A737FF'
+    cell.style.color = (player == 'x') ? '#1892EA' : '#A737FF'
 }
+
+function changePlayer() {
+    player = (player == 'x') ? 'o' : 'x'
+}
+
+function randomPick() {
+    isPauseGame = true
+
+    setTimeout(() => {
+        let randomIndex
+        do {
+            randomIndex = Math.floor(Math.random() * inputCells.length)
+        } while (
+            inputCells[randomIndex] != ''
+        )
+
+
+        updateCell(cells[randomIndex], randomIndex, player)
+        if (!checkWinner()) {
+            changePlayer()
+            isPauseGame = false
+            return
+        }
+        player = (player == 'x') ? 'o' : 'x'
+    }, 1000)
+}
+
+function checkWinner() {
+    for (const [a, b, c] of winConditions) {
+        if (inputCells[a] == player &&
+            inputCells[b] == player &&
+            inputCells[c] == player
+        ) {
+            declareWinner([a, b, c])
+            return true
+        }
+    }
+
+
+    if (inputCells.every(cell => cell != '')) {
+        declareDraw()
+        return true
+    }
+}
+
+function declareWinner(winningIndices) {
+    titleHeader.textContent = `${player} Win`
+    isPauseGame = true
+
+
+    winningIndices.forEach((index) => 
+        cells[index].style.background = '#2A2343'
+    )
+
+    restartBtn.style.visibility = 'visible'
+}
+
+
+function declareDraw() {
+    titleHeader.textContent = 'Draw!'
+    isPauseGame = true
+    restartBtn.style.visibility = 'visible'
+}
+
+function choosePlayer(selectedPlayer) {
+    if (!isGameStart) {
+        player = selectedPlayer
+        if (player == 'x') {
+            xPlayerDisplay.classList.add('player-active')
+            oPlayerDisplay.classList.remove('player-active')
+        } else {
+
+            xPlayerDisplay.classList.remove('player-active')
+            oPlayerDisplay.classList.add('player-active')
+        }
+    }
+}
+
+restartBtn.addEventListener('click', () => {
+    restartBtn.style.visibility = 'hidden'
+    inputCells.fill('')
+    cells.forEach(cell => {
+        cell.textContent = ''
+        cell.style.background = ''
+    })
+    isPauseGame = false
+    isGameStart = false
+    titleHeader.textContent = 'Choose'
+})
